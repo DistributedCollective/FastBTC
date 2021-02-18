3c41f31339d5e95e7053362f3acc838578f7a1ef
Bitcoinera <anagjordano@gmail.com>
2021-02-18 18:56:59 +0100

    do not default to conf mainnet

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 import conf_mainnet from "./config-main";
 import conf_testnet from "./config-test";
 
-let config = conf_mainnet;
+let config;
 if (process.argv && process.argv[2]=="mainnet") config = conf_mainnet;
+else config = conf_testnet
 
 export default config;
