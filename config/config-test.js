78102999bf3f160aeddab42c24e60408b93896eb
Antti Haapala <antti.haapala@interjektio.fi>
2021-04-12 21:13:40 +0300

    configure the fastbtc endpoint in synthetic javascript

--- config/config-test.js
+++ config/config-test.js
@@ -5,8 +5,9 @@ import accounts from "../secrets/accounts";
 import slaves from '../secrets/slaves';
 
 export default {
-    env: "test", 
+    env: "test",
     serverPort: 3007,
+    backendUrl: "http://localhost:3007",
     appName: "FastBtcV3",
     dbName: "fastbtcv3_test",
     rskNode: "https://testnet.sovryn.app/rpc",
@@ -17,10 +18,10 @@ export default {
     telegramBot: Object.keys(telegramBot).length > 0 ? telegramBot : null,
     telegramGroupId: -523868176,
     walletSigs: walletSigs,
-    slaves: slaves.test, 
+    slaves: slaves.test,
     contractAddress: "0xcC099752238b1932587bf5793Afeb7d80D04F6e1".toLowerCase(),
     multisigAddress: "0x1D67BDA1144CacDbEFF1782f0E5B43D7B50bbFe0".toLowerCase(),
     account: accounts["test"],
     node: node.test,
     thresholdConfirmations: 1
-}
+};
--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ import conf_mainnet from "./config-main";
 import conf_testnet from "./config-test";
 
 let config = conf_testnet;
-if (process.argv && process.argv[2]=="mainnet") config = conf_mainnet;
+if (process.argv && process.argv[2]=="mainnet") {
+    config = conf_mainnet;
+}
 
 export default config;
