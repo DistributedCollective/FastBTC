4c4ddc127dc6df28d15cdf276c37e53b5712397b
Antti Haapala <antti.haapala@interjektio.fi>
2021-04-25 14:05:31 +0300

    Allow overriding configs locally in config-xxxx.overrides.js. These will be ignored in .gitignore.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,23 @@
-import conf_mainnet from "./config-main";
-import conf_testnet from "./config-test";
+import confMainnet from "./config-main";
+import confTestnet from "./config-test";
 
-let config = conf_testnet;
+let config = confTestnet;
+let overrideModule = "./config-test.override.js";
+
 if (process.argv && process.argv[2]=="mainnet") {
-    config = conf_mainnet;
+    config = confMainnet;
+    overrideModule = "./config-main.override.js";
 }
 
+try {
+    console.log("attempting to load overrides from %s", overrideModule);
+    const extraConfig = require(overrideModule);
+    config = {...config, ...extraConfig['default']}
+    console.log("success");
+}
+catch (e) {
+    console.log("override module not available");
+}
+
+console.log(config);
 export default config;
