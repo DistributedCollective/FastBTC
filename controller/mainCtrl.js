15ea991899c7212e88c6d5c92a4c28eb83170a24
Antti Haapala <antti.haapala@interjektio.fi>
2021-11-27 02:12:04 +0200

    fix anchor

--- controller/mainCtrl.js
+++ controller/mainCtrl.js
@@ -73,7 +73,7 @@ class MainController {
 
             // hem, the address is given as an array of 1 string
             address = address.toString().toLowerCase();
-            if (! /(?:bsc|bsctest:)?^0x[a-f0-9]{40}$/.test(address)) {
+            if (! /^(?:bsc|bsctest:)?0x[a-f0-9]{40}$/.test(address)) {
                 return cb({error: "Malformed EVM address"});
             }
 
