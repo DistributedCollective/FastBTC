027e60f7692c7c9cd5de58f0942001aff808819d
Antti Haapala <antti.haapala@interjektio.fi>
2022-06-01 19:09:48 +0300

    new BSC aggregator address; better pm2 config.

--- config/config-test.js
+++ config/config-test.js
@@ -7,14 +7,14 @@ import slaves from '../secrets/slaves';
 export default {
     env: "test",
     serverPort: 3007,
-    backendUrl: "http://3.129.31.108:3007/",
+    backendUrl: "http://3.132.40.118:3007/",
     appName: "FastBtcV3",
     dbName: "fastbtcv3_test",
     rskNode: "https://testnet.sovryn.app/rpc",
     blockExplorer: "https://explorer.testnet.rsk.co",
     commission: 500, //in sats
-    minAmount: 10000, //sats, = 0.001 btc
-    maxAmount: 1000000, //sats, = 0.1 btc
+    minAmount: 2000, //sats, = 0.00002 btc
+    maxAmount: 1000000, //sats, = 0.01000000 btc
     balanceThreshold: 0.05, //in rbtc
     telegramBot: Object.keys(telegramBot).length > 0 ? telegramBot : null,
     telegramGroupId: -523868176,
@@ -24,7 +24,9 @@ export default {
     multisigAddress: "0x1D67BDA1144CacDbEFF1782f0E5B43D7B50bbFe0".toLowerCase(),
     contractAddress: "0x8dC7B212692b3E36aF7E8202F06516d0dB3Bf1B6".toLowerCase(),
     bscBridgeAddress: "0x2b2bcad081fa773dc655361d1bb30577caa556f8".toLowerCase(),
-    bscAggregatorAddress: "0xe2C2fbAa4407fa8BB0Dbb7a6a32aD36f8bA484aE".toLowerCase(),
+    bscAggregatorAddress: "0xc54B47AC178273A42Fb71631d8018aD7EBbec330".toLowerCase(),
+
+    // old: bscAggregatorAddress: "0xe2C2fbAa4407fa8BB0Dbb7a6a32aD36f8bA484aE".toLowerCase(),
     bscPrefix: 'bsctest:',
 
     account: accounts["test"],
